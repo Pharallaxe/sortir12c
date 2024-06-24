61c66cbc073e453659570593ae2deac7b41bebf8
sand-del <sandradelimoge@gmail.com>
2024-06-24 16:07:41 +0200

    version de départ

--- public/index.php
+++ public/index.php
@@ -7,7 +7,3 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
-//voila mon super commentaire !!! :)
-//test ada
-// Il fait beau, les oiseaux chantent !
