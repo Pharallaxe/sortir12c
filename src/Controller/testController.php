fb1dbc21f8a36b4545831272d8859407f949aef9
2ant <52797081+2ant@users.noreply.github.com>
2024-06-25 14:42:50 +0200

    ajout page test et nav

---
+++ src/Controller/testController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class testController extends AbstractController
+{
+    #[Route('/test')]
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig');
+    }
+}
