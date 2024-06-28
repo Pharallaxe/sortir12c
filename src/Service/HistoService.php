ee1be8964cdb0a30850239e26ec357e86a096a06
Pharallaxe <103119043+Pharallaxe@users.noreply.github.com>
2024-06-28 21:50:52 +0200

    Mise au propre du HistoService

--- src/Service/HistoService.php
+++ src/Service/HistoService.php
@@ -9,16 +9,19 @@ use Doctrine\ORM\EntityManagerInterface;
 class HistoService
 {
 
+    private $sortieRepository;
+    private $etatRepository;
+    private $em;
 
-
-    public function __construct(private SortieRepository $sortieRepository,
-                                private EtatRepository $etatRepository,
-                                private EntityManagerInterface $_em)
+    public function __construct(
+        SortieRepository $sortieRepository,
+        EtatRepository $etatRepository,
+        EntityManagerInterface $em)
     {
-
+        $this->sortieRepository = $sortieRepository;
+        $this->etatRepository = $etatRepository;
+        $this->em = $em;
     }
-
-    // SortieRepository.php
 
     public function update(): void
     {
@@ -26,30 +29,18 @@ class HistoService
         $etat = $this->etatRepository->find(5);
 
         foreach ($sorties as $sortie) {
-            // Get the date one month ago from now
             $oneMonthAgo = new \DateTime('now');
             $oneMonthAgo->modify('-1 month');
-
-            // Ensure $oneMonthAgo is set to midnight to accurately compare dates
             $oneMonthAgo->setTime(0, 0, 0);
-
-            // Get the dateHeureDebut of Sortie and format it for comparison
             $dateHeureDebut = $sortie->getDateHeureDebut();
             $dateHeureDebutFormatted = $dateHeureDebut->format('Y-m-d H:i:s');
 
-            // Compare dateHeureDebut with oneMonthAgo
             if ($dateHeureDebut < $oneMonthAgo) {
-                // Update the etat of Sortie
                 $sortie->setEtat($etat);
-
-                // Persist changes
-                $this->_em->persist($sortie);
+                $this->em->persist($sortie);
             }
         }
 
-        // Flush all changes
-        $this->_em->flush();
+        $this->em->flush();
     }
-
-
 }
