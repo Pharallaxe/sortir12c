8e69412e91a9eb02e5566858d9ee375f87dbf1a1
sand-del <sandradelimoge@gmail.com>
2024-06-28 15:39:34 +0200

    login pseudo ou email

--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Participant;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -12,7 +13,7 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 /**
  * @extends ServiceEntityRepository<Participant>
  */
-class ParticipantRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class ParticipantRepository extends ServiceEntityRepository implements PasswordUpgraderInterface, UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
@@ -32,4 +33,15 @@ class ParticipantRepository extends ServiceEntityRepository implements PasswordU
         $this->getEntityManager()->persist($user);
         $this->getEntityManager()->flush();
     }
+
+public function loadUserByIdentifier(string $emailOuPseudo): ?Participant
+{
+    return $this->createQueryBuilder('p')
+        ->where('p.email = :identifier')
+        ->orWhere('p.pseudo = :identifier')
+        ->setParameter('identifier', $emailOuPseudo)
+        ->setMaxResults(1)
+        ->getQuery()
+        ->getSingleResult();
+}
 }
