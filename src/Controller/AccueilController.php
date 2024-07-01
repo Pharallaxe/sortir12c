8beb2e398c81f0bd37ec0e599caf519f056f889b
Pharallaxe <103119043+Pharallaxe@users.noreply.github.com>
2024-07-01 11:51:57 +0200

    Ajout du AccueilController.

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,19 +1,16 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class testController extends AbstractController
+class AccueilController extends AbstractController
 {
-
-    #[Route('/test', name: 'site_accueil')]
+    #[Route('/', name: 'site_accueil')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig');
+        return $this->render('accueil/acceuil.html.twig');
     }
 }
